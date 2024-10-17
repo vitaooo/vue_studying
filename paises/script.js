c4b2d5c88f2e64190ba97da4d945ae53b2f34bc5
Victor <victorhvercosag@gmail.com>
2024-10-17 17:34:10 -0300

    components

--- paises/script.js
+++ paises/script.js
@@ -2,19 +2,18 @@ function bandeira(pais) {
     return '<img src="'+pais+'.png" />';
 }
 
+Vue.component("pais", {
+    props:['bandeira', 'continente'],
+    template:`<p v-html="bandeira + continente"></p>`
+});
+
 let app = new Vue({
     el:'#app',
     data:{
-        pais:'',
         argentina:bandeira('argentina'),
         brazil:bandeira('brazil'),
         china:bandeira('china'),
         uk:bandeira('uk'),
-        usa:bandeira('usa')
-    },
-    methods:{
-
+        usa:bandeira('usa'),
     }
 });
-
-app.pais = 'usa';
