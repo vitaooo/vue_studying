88d8f9339d0447113c4e7dfdf446afd6ff0867a8
Victor <victorhvercosag@gmail.com>
2024-10-17 17:48:30 -0300

    before ciclo de vida

--- paises/script.js
+++ paises/script.js
@@ -1,19 +1,9 @@
-function bandeira(pais) {
-    return '<img src="'+pais+'.png" />';
-}
-
-Vue.component("pais", {
-    props:['bandeira', 'continente'],
-    template:`<p v-html="bandeira + continente"></p>`
-});
-
 let app = new Vue({
     el:'#app',
     data:{
-        argentina:bandeira('argentina'),
-        brazil:bandeira('brazil'),
-        china:bandeira('china'),
-        uk:bandeira('uk'),
-        usa:bandeira('usa'),
+        nome:'vintão'
+    },
+    beforeCreate:function(){
+        alert(this.nome);
     }
 });
