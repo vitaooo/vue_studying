7c3d90609febf81439fee89b287bb62af9145dd0
Victor <victorhvercosag@gmail.com>
2024-10-21 10:00:19 -0300

    interpolações

--- paises/script.js
+++
@@ -1,9 +0,0 @@
-let app = new Vue({
-    el:'#app',
-    data:{
-        nome:'vintão'
-    },
-    beforeCreate:function(){
-        alert(this.nome);
-    }
-});
---
+++ script.js
@@ -0,0 +1,12 @@
+let app = new Vue({
+    el:'#app',
+    data:{
+        titulo:'Título da page',
+        subtitulo:'Esse é o subtítulo da página',
+        copyright:'Todos os direitos reservados!',
+
+        numero:10,
+
+        carregado:true
+    }
+});
