64237ff3a5f524b4b41b9b6d3c27fd7ce931fcf7
Victor <victorhvercosag@gmail.com>
2024-10-22 14:11:20 -0300

    watcher

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 let app = new Vue({
     el:'#app',
     data:{
-        nome:'Vintor'
+        conta:'',
+        aviso:'',
+        resultado:'',
+        timer:null
     },
-    computed:{
-        nomeInvertido:function() {
-            return this.nome.split('').reverse().join('');
+    methods:{
+        fazerConta:function() {
+            this.aviso = '';
+
+            this.resultado = eval(this.conta);
+        }
+    },
+    watch:{
+        conta:function() {
+            this.aviso = 'Digitando...'
+
+            if(this.timer != null) {
+                clearTimeout(this.timer);
+            }
+            this.timer = setTimeout(this.fazerConta, 1000);
         }
     }
 });
